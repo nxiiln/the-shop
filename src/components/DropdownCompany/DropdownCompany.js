7643b6ac38656a707eb6fd60df2ded3ee6ac8fe6
nxiiln <barbashin.nv@gmail.com>
2021-12-11 02:02:34 +0300

    DropdownCompany component changed renderDropdown, handleMouseEnter and handleMouseLeave removed

--- src/components/DropdownCompany/DropdownCompany.js
+++ src/components/DropdownCompany/DropdownCompany.js
@@ -4,28 +4,25 @@ import styles from './dropdownCompany.module.scss';
 
 const DropdownCompany = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const handleMouseEnter = () => setIsOpen(true);
-  const handleMouseLeave = () => setIsOpen(false);
-
-  const renderDropdown = () => {
-    const list = ['about us', 'contact', 'store location'];
-    return list.map(item => {
-      return(
-        <div key={item} className={styles.item}>
-          {item}
-        </div>
-      );
-    });
-  }
+  const list = ['about us', 'contact', 'store location'];
 
   return(
     <div
       className={styles.dropdown}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
     >
-      <div className={styles.itemMain}>company &#9660;</div>
-      {isOpen && renderDropdown()}
+      <div className={styles.itemMain}>
+        company &#9660;
+      </div>
+
+      {isOpen && list.map(item => {
+        return(
+          <div key={item} className={styles.item}>
+            {item}
+          </div>
+        );
+      })}
     </div>
   );
 }
