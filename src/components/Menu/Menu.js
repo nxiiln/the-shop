cd4b659ed095105e3abfbb1e87cd0da98dc7ef96
nxiiln <barbashin.nv@gmail.com>
2021-12-20 15:18:31 +0300

    Menu component changed added spaces and indents to JSX

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -13,59 +13,63 @@ const Menu = () => {
     <div
       className={styles.menu}
     >
-      <ul className={mainMenu}>
-        <li
+      <nav>
+        <ul className={mainMenu}>
+          <li
+            onMouseEnter={() => setMenuOpen(true)}
+            onMouseLeave={() => setMenuOpen(false)}
+          >
+            women
+          </li>
+          <li>men</li>
+          <li>kids</li>
+          <li>accessories</li>
+          <li>whats new</li>
+          <li>brands</li>
+          <li>sale</li>
+          <li>blog</li>
+        </ul>
+      </nav>
+
+      {menuOpen &&
+        <div
+          className={styles.menuOpen}
           onMouseEnter={() => setMenuOpen(true)}
           onMouseLeave={() => setMenuOpen(false)}
         >
-          women
-        </li>
-        <li>men</li>
-        <li>kids</li>
-        <li>accessories</li>
-        <li>whats new</li>
-        <li>brands</li>
-        <li>sale</li>
-        <li>blog</li>
-      </ul>
-      {menuOpen &&
-      <div
-        className={styles.menuOpen}
-        onMouseEnter={() => setMenuOpen(true)}
-        onMouseLeave={() => setMenuOpen(false)}
-      >
-        <div className={styles.menuInnerWrapper}>
-          <ul className={styles.menuInner}>
-            <li>bottoms</li>
-            <li>Jeans</li>
-            <li>Pants</li>
-            <li>Shorts</li>
-            <li>Skirts</li>
-            <li>Dresses</li>
-          </ul>
-          <ul className={styles.menuInner}>
-            <li>tops</li>
-            <li>Jackets & Coats</li>
-            <li>Shirts</li>
-            <li>T-shirts</li>
-            <li>Knitwear</li>
-            <li>Sweats</li>
-          </ul>
-          <ul className={styles.menuInner}>
-            <li>shoes & more</li>
-            <li>Shoes</li>
-            <li>Underwear</li>
-            <li>Accessories</li>
-            <li>Collectables</li>
-            <li>Eyewear</li>
-          </ul>
-          <ul className={styles.menuInner}>
-            <li>collections</li>
-            <li>New arrivals</li>
-            <li>Urban Style</li>
-            <li>Raw Correct</li>
-          </ul>
-        </div>
+          <div className={styles.menuInnerWrapper}>
+            <ul className={styles.menuInner}>
+              <li>bottoms</li>
+              <li>Jeans</li>
+              <li>Pants</li>
+              <li>Shorts</li>
+              <li>Skirts</li>
+              <li>Dresses</li>
+            </ul>
+            <ul className={styles.menuInner}>
+              <li>tops</li>
+              <li>Jackets & Coats</li>
+              <li>Shirts</li>
+              <li>T-shirts</li>
+              <li>Knitwear</li>
+              <li>Sweats</li>
+            </ul>
+            <ul className={styles.menuInner}>
+              <li>shoes & more</li>
+              <li>Shoes</li>
+              <li>Underwear</li>
+              <li>Accessories</li>
+              <li>Collectables</li>
+              <li>Eyewear</li>
+            </ul>
+            <ul className={styles.menuInner}>
+              <li>collections</li>
+              <li>New arrivals</li>
+              <li>Urban Style</li>
+              <li>Raw Correct</li>
+            </ul>
+          </div>
+
           <div className={styles.imageWrapper}>
             <img
               src={imageMenuInner}
@@ -78,7 +82,7 @@ const Menu = () => {
               </p>
             </div>
           </div>
-      </div>
+        </div>
       }
     </div>
   )
