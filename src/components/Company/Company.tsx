dbce6723730bf2c1d01cc0f936db3795bc988789
nxiiln <barbashin.nv@gmail.com>
2022-01-09 00:13:54 +0300

    The styles for the Company component are rewritten in styled-components

--- src/components/Company/Company.tsx
+++ src/components/Company/Company.tsx
@@ -1,47 +1,83 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
 import styles from './company.module.scss';
 
 
+type Open = {open: boolean};
+
+const Dropdown = styled.div<Open>`
+  width: 83px;
+  height: 36px;
+  position: relative;
+  ${props => props.open && `
+    height: 129px;
+    background-color: #333;
+  `}
+`;
+
+const Button = styled.button`
+  width: 54px;
+  height: 12px;
+  margin: 0 0 8px 14px;
+  padding: 0;
+  font-family: Nunito;
+  font-size: 10px;
+  text-transform: uppercase;
+  text-align: start;
+  vertical-align: baseline;
+  color: #aaa;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    color: #fff;
+  }
+`;
+
+const ButtonMain = styled(Button)`
+  margin: 12px 0 12px 14px;
+`;
+
+const Triangle = styled.div`
+  width: 0;
+  height: 0;
+  position: absolute;
+  top: 17px;
+  left: 71px;
+  border-top: 4px solid #aaa;
+  border-right: 3px solid transparent;
+  border-left: 3px solid transparent;
+`;
+
+
 const Company = (): JSX.Element => {
-  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   const list: string[] = ['about us', 'contact', 'store location'];
 
-  let dropdown: string = styles.dropdown;
-  let buttonMain: string = styles.buttonMain;
-  let button: string = styles.button;
-  
-  if (dropdownOpen) {
-    dropdown += ` ${styles.dropdownOpen}`;
-    buttonMain += ` ${styles.buttonOpen}`;
-    button += ` ${styles.buttonOpen}`;
-  }
-
   return(
-    <div
-      className={dropdown}
-      onMouseLeave={(): void => setDropdownOpen(false)}
+    <Dropdown
+      open={open}
+      onMouseLeave={(): void => setOpen(false)}
     >
-      <button
-        className={buttonMain}
+      <ButtonMain
         type='button'
-        onMouseEnter={(): void => setDropdownOpen(true)}
+        onMouseEnter={(): void => setOpen(true)}
       >
         company
-      </button>
-      <div className={styles.triangle}></div>
+      </ButtonMain>
+      <Triangle />
 
-      {dropdownOpen && list.map((item: string): JSX.Element => {
+      {open && list.map((item: string): JSX.Element => {
         return(
-          <button
-            className={button}
+          <Button
             type='button'
             key={item}
           >
             {item}
-          </button>
+          </Button>
         );
       })}
-    </div>
+    </Dropdown>
   );
 }
 
