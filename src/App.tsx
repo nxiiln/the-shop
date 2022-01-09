c08dcee587ad4b02c27921e1b9552805ce695e05
nxiiln <barbashin.nv@gmail.com>
2022-01-10 02:56:18 +0300

    Styles removed from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import './app.scss';
 
 
-const App = () => {
+const App = (): JSX.Element => {
   return(
     <div>
       <Header />
