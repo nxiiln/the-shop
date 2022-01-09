3eb55a881b603ba6785b76b5e2cacdb8c87328ad
nxiiln <barbashin.nv@gmail.com>
2022-01-10 00:11:33 +0300

    The styles for the Top component have been rewritten in styled-components

--- src/components/Top/Top.tsx
+++ src/components/Top/Top.tsx
@@ -2,58 +2,104 @@ import React from 'react';
 import Company from '../Company/Company';
 import Currency from '../Currency/Currency';
 import Language from '../Language/Language';
-import styles from './top.module.scss';
+import styled from 'styled-components';
+
+
+const Nav = styled.nav`
+  width: 100vw;
+  height: 36px;
+  display: flex;
+  justify-content: space-evenly;
+  background-color: #000;
+`;
+
+const GroupLeft = styled.div`
+  width: 223px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GroupCenter = styled.div`
+  padding-top: 12px;
+  padding-left: 5vw;
+  font-family: Nunito;
+  font-size: 10px;
+  text-transform: uppercase;
+  color: #fff;
+`;
+
+const GroupRight = styled.div`
+  width: 295px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Line = styled.div`
+  width: 1px;
+  height: 8px;
+  background-color: #4f4f4f;
+`;
+
+const Button = styled.button`
+  height: 12px;
+  display: block;
+  padding: 0;
+  font-family: Nunito;
+  font-size: 10px;
+  text-transform: uppercase;
+  text-align: start;
+  line-height: 0;
+  color: #aaa;
+  background-color: #000;
+  border: none;
+  &:hover {
+    color: #fff;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+const MyAccount = styled(Button)`
+  width: 72px;
+`;
+
+const WishList = styled(Button)`
+  width: 54px;
+`;
+
+const Checkout = styled(Button)`
+  width: 58px;
+`;
+
+const LogIn = styled(Button)`
+  width: 38px;
+`;
 
 
 const Top = (): JSX.Element => {
   return(
-    <nav className={styles.top}>
-      <div className={styles.groupLeft}>
+    <Nav>
+      <GroupLeft>
         <Company />
         <Currency />
         <Language />
-      </div>
+      </GroupLeft>
 
-      <div className={styles.groupCenter}>
+      <GroupCenter>
         free shipping on orders above 50$
-      </div>
+      </GroupCenter>
 
-      <div className={styles.groupRight}>
-        <button
-          className={styles.buttonMyAccount}
-          type='button'
-        >
-          my account
-        </button>
-
-        <div className={styles.line}></div>
-
-        <button
-          className={styles.buttonWishList}
-          type='button'
-        >
-          wish list
-        </button>
-
-        <div className={styles.line}></div>
-
-        <button
-          className={styles.buttonCheckout}
-          type='button'
-        >
-          checkout
-        </button>
-
-        <div className={styles.line}></div>
-
-        <button
-          className={styles.buttonLogIn}
-          type='button'
-        >
-          log in
-        </button>
-      </div>
-    </nav>
+      <GroupRight>
+        <MyAccount type='button'>my account</MyAccount>
+        <Line />
+        <WishList type='button'>wish list</WishList>
+        <Line />
+        <Checkout type='button'>checkout</Checkout>
+        <Line />
+        <LogIn type='button'>log in</LogIn>
+      </GroupRight>
+    </Nav>
   );
 }
 
