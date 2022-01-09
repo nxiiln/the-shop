a19579b7e3d890ef056f2cbfd56651a9d25ed2e6
nxiiln <barbashin.nv@gmail.com>
2022-01-10 00:59:09 +0300

    Home component rewritten to TypeScript

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
 import Banner from '../Banner/Banner';
 import WhatsNew from '../WhatsNew/WhatsNew';
-import styles from './home.module.scss';
 
 
-const Home = () => {
+const Home = (): JSX.Element => {
   return(
     <div>
       <Banner />
