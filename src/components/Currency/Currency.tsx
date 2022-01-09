f94d6839e5731e29e0535bfcfc8570f1c6b44c09
nxiiln <barbashin.nv@gmail.com>
2022-01-09 22:55:32 +0300

    The styles for the Currency component have been rewritten in styled-components

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,46 +1,82 @@
 import React, {useState} from 'react';
-import styles from './currency.module.scss';
+import styled from 'styled-components';
+
+
+type Open = {open: boolean};
+
+const Dropdown = styled.div<Open>`
+  width: 53px;
+  height: 36px;
+  position: relative;
+  ${props => props.open && `
+    height: 68px;
+    background-color: #333;
+  `}
+`;
+
+const Button = styled.button`
+  width: 24px;
+  height: 12px;
+  margin-left: 14px;
+  padding: 0;
+  font-family: Nunito;
+  font-size: 10px;
+  text-transform: uppercase;
+  text-align: start;
+  vertical-align: baseline;
+  color: #aaa;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    color: #fff;
+  }
+`;
+
+const ButtonMain = styled(Button)`
+  width: 38px;
+  margin: 12px 0 12px 14px;
+`;
+
+const Triangle = styled.div`
+  width: 0;
+  height: 0;
+  position: absolute;
+  top: 17px;
+  left: 41px;
+  border-top: 4px solid #aaa;
+  border-right: 3px solid transparent;
+  border-left: 3px solid transparent;
+`;
 
 
 const Currency = (): JSX.Element => {
   const [open, setOpen] = useState<boolean>(false);
   const [isUsd, setIsUsd] = useState<boolean>(true);
 
-  let dropdown: string = styles.dropdown;
-  let buttonMain: string = styles.buttonMain;
-  let button: string = styles.button;
-  
-  if (open) {
-    dropdown += ` ${styles.open}`;
-    buttonMain += ` ${styles.buttonOpen}`;
-    button += ` ${styles.buttonOpen}`;
-  }
-
   return(
-    <div
-      className={dropdown}
+    <Dropdown
+      open={open}
       onClick={(): void => setOpen(false)}
       onMouseLeave={(): void => setOpen(false)}
     >
-      <button
-        className={buttonMain}
+      <ButtonMain
         type='button'
         onMouseEnter={(): void => setOpen(true)}
       >
         {isUsd ? 'usd' : 'eur'}
-      </button>
-      <div className={styles.triangle}></div>
+      </ButtonMain>
+      <Triangle />
 
       {open &&
-        <button
-          className={button}
+        <Button
           type='button'
           onClick={(): void => setIsUsd(!isUsd)}
         >
           {isUsd ? 'eur' : 'usd'}
-        </button>
+        </Button>
       }
-    </div>
+    </Dropdown>
   );
 }
 
