2a88f60959aa7b9c86eaf52ceae6c2af723275b2
nxiiln <barbashin.nv@gmail.com>
2022-01-14 08:21:43 +0300

    <Footer /> added to the App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
 
 
 const App = (): JSX.Element => {
@@ -8,6 +9,7 @@ const App = (): JSX.Element => {
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
