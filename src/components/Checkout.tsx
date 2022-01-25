c19797b362a3cf358bf568f293469ef7a825f0c6
nxiiln <barbashin.nv@gmail.com>
2022-01-25 22:03:12 +0300

    Created styled components for <AlsoLove>

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -12,24 +12,25 @@ const WrapperOuter = styled.div`
 `;
 
 const WrapperInner = styled.div`
-  max-width: 960px;
+  width: 960px;
   height: 1740px;
-  display: grid;
-  grid-template-columns: 685px 1fr;
-  grid-template-rows: 10px 27px 325px 1fr;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: start;
   border: 1px solid orange;
 `;
 
 const Line = styled.div`
   width: 100%;
   height: 1px;
-  grid-area: 1 / 1 / 2 / 3;
+  margin-bottom: 10px;
   background: #aaa;
 `;
 
 const BreadCrumbsWrapper = styled.div`
   width: 100%;
-  grid-area: 2 / 1 / 3 / 3;
+  margin-bottom: 30px;
   display: flex;
   justify-content: space-between;
 `;
@@ -55,30 +56,145 @@ const BreadCrumbsRight = styled(BreadCrumbs)`
   height: 10px;
 `;
 
+
+//Also Love--------------------------------------
 const AlsoLove = styled.div`
   width: 960px;
   height: 296px;
-  grid-area: 3 / 1 / 4 / 3;
+  margin-bottom: 30px;
+  position: relative;
   background-color: #f7f7f7;
   border: 1px solid #e4e2e1;
+  > span:first-child {
+    position: relative;
+    top: 25px;
+    left: 22px;
+    font-family: 'Playfair Display SC';
+    font-size: 21px;
+    font-weight: 400;
+    color: #000;
+  }
 `;
 
+const AlsoLoveClose = styled.button`
+  width: 29px;
+  height: 29px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: #fff;
+  border: 1px solid #e4e2e1;
+  cursor: pointer;
+  > span:first-child  {
+    display: inline-block;
+    position: absolute;
+    top: -5px;
+    left: 7px;
+    font-family: Nunito;
+    font-size: 25px;
+    color: #000;
+    transform: rotate(45deg);
+  }
+`;
+
+const ProductAlsoWrapper = styled.div`
+  width: 890px;
+  height: 190px;
+  position: absolute;
+  top: 75px;
+  left: 25px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductAlso = styled.div`
+  width: 260px;
+  height: 190px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  > span:nth-child(2) {
+    width: 115px;
+    margin: 0 0 20px 10px;
+    font-family: 'Playfair Display SC';
+    font-size: 14px;
+    line-height: 18px;
+    font-weight: 400;
+    text-transform: uppercase;
+    color: #000;
+  }
+  > span:nth-child(3) {
+    margin: 0 0 25px 10px;
+    font-family: Nunito;
+    font-size: 12px;
+    line-height: 1.2;
+    font-weight: 300;
+    color: #000;
+  }
+`;
+
+const QuickView = styled.button`
+  width: 107px;
+  height: 30px;
+  margin-left: 10px;
+  font-family: Nunito;
+  font-size: 10px;
+  color: #000;
+  background: #fff;
+  border: 1px solid #e4e2e1;
+  cursor: pointer;
+`;
+//-----------------------------------------------
+
+
+//Step-------------------------------------------
 const Step1 = styled.div`
   width: 675px;
   height: 345px;
-  grid-area: 4 / 1 / 5 / 2;
   border: 1px solid #e4e2e1;
 `;
 
 const Cart = styled.div`
   width: 275px;
   height: 478px;
-  grid-area: 4 / 2 / 5 / 3;
   border: 1px solid #e4e2e1;
 `;
+//-----------------------------------------------
 
 
 const Checkout = (): JSX.Element => {
+interface ProductAlso {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+}
+
+const productsAlso: ProductAlso[] = [
+  {
+    id: 1,
+    image: alsoLove1,
+    name: 'detailed swing dress',
+    price: 1875
+  },
+  {
+    id: 2,
+    image: alsoLove2,
+    name: 'detailed swing dress',
+    price: 850
+  },
+  {
+    id: 3,
+    image: alsoLove3,
+    name: 'detailed swing dress',
+    price: 875
+  }
+];
+
+
+const [alsoLove, setAlsoLove] = useState<boolean>(true);
+
+
   return(
     <WrapperOuter>
       <WrapperInner>
@@ -92,8 +208,31 @@ const Checkout = (): JSX.Element => {
           </BreadCrumbsRight>
         </BreadCrumbsWrapper>
 
-        <AlsoLove>
-        </AlsoLove>
+        {alsoLove && 
+          <AlsoLove>
+            <span>YOU WILL ALSO LOVE</span>
+            <AlsoLoveClose
+              type='button'
+              onClick={(): void => setAlsoLove(false)}
+            >
+              <span>+</span>
+            </AlsoLoveClose>
+            <ProductAlsoWrapper>
+              {productsAlso.map((product: ProductAlso): JSX.Element => {
+                return(
+                  <ProductAlso key={product.id}>
+                    <img src={product.image} alt={product.name} />
+                    <span>{product.name}</span>
+                    <span>
+                      ${product.price.toString().replace(/(.{1,3})(...)$/, '$1,$2')}
+                    </span>
+                    <QuickView>QUICKVIEW</QuickView>
+                  </ProductAlso>
+                )
+              })}
+            </ProductAlsoWrapper>
+          </AlsoLove>
+        }
 
         <Step1>
         </Step1>
@@ -102,7 +241,7 @@ const Checkout = (): JSX.Element => {
         </Cart>
       </WrapperInner>
     </WrapperOuter>
-  );
+  )
 }
 
 
