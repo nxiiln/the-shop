447cfa4296e7241bb182942caf5ab68a0aa00290
nxiiln <barbashin.nv@gmail.com>
2022-02-05 08:37:33 +0300

    QuickView component created

---
+++ src/components/QuickView.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+  width: 700px;
+  height: 430px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background: #fff;
+  border: 1px solid #e4e2e1;
+  z-index: 1;
+`;
+
+
+const QuickView = (): JSX.Element => {
+  return(
+    <Wrapper>
+    </Wrapper>
+  )
+}
+
+
+export default QuickView;
