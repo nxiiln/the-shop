7950fb7226eafa927496836d836fc73d3bb5b6a1
nxiiln <barbashin.nv@gmail.com>
2022-02-06 08:54:31 +0300

    Description block added to QuickView component

--- src/components/QuickView.tsx
+++ src/components/QuickView.tsx
@@ -1,12 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import quickViewImage from '../images/quickViewImage.png'
+import wishList from '../images/wishList.png'
+import compare from '../images/compare.png'
 
 
 
 
-const WrapperOuter = styled.article<{display: boolean}>`
-  ${props => !props.display && 'display: none;'}
+const WrapperOuter = styled.article<{status: boolean}>`
+  ${props => !props.status && 'display: none;'}
   width: 700px;
   height: 430px;
   position: absolute;
@@ -75,10 +77,113 @@ const DescriptionWrapper = styled.div`
   width: 328px;
   height: 278px;
   margin-left: 48px;
-  border: 1px solid hotpink;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
 `;
 
+const Index = styled.span`
+  font-family: Arial;
+  font-size: 11px;
+  font-weight: 400;
+  color: #aaa;
+`;
 
+const Name = styled.span`
+  margin-bottom: 15px;
+  font-family: 'Playfair Display SC';
+  font-size: 24px;
+  font-weight: 400;
+  color: #000;
+`;
+
+const Details = styled.span`
+  margin-bottom: 20px;
+  font-family: Arial;
+  font-size: 11px;
+  font-weight: 400;
+  color: #000000;
+`;
+
+const Price = styled.span`
+  margin-bottom: 15px;
+  font-family: Nunito;
+  font-size: 18px;
+  font-weight: 300;
+  color: #000;
+`;
+
+const Select = styled.select<{attr: string}>`
+  width: ${props => props.attr === 'qty' ? '94px' : '325px'};
+  height: 29px;
+  margin-bottom: ${props => props.attr === 'qty' ? '20px' : '5px'};
+  padding-left: 12px;
+  font-family: Nunito;
+  font-size: 11px;
+  line-height: 1.2;
+  font-weight: 300;
+  text-transform: uppercase;
+  color: #000;
+  background: #f7f7f7;
+  border: 1px solid #e4e2e1;
+  border-radius: 15px;
+`;
+
+const ButtonsWrapper = styled.div`
+  width: 328px;
+  height: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const AddToCart = styled.button`
+  width: 103px;
+  height: 30px;
+  font-family: Nunito;
+  font-size: 10px;
+  font-weight: 300;
+  color: #fff;
+  background: #000;
+  border: none;
+  cursor: pointer;
+`;
+
+const WishList = styled.button`
+  width: 107px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Nunito;
+  font-size: 10px;
+  font-weight: 400;
+  color: #000;
+  background: #fff;
+  border: 1px solid #e4e2e1;
+  cursor: pointer;
+  > img {
+    margin-right: 5px;
+  }
+`;
+
+const Compare = styled.button`
+  width: 107px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Nunito;
+  font-size: 10px;
+  font-weight: 400;
+  color: #000;
+  background-color: #f7f7f7;
+  border: 1px solid #e4e2e1;
+  cursor: pointer;
+  > img {
+    margin-right: 5px;
+  }
+`;
 //-----------------------------------------------
 
 
@@ -87,8 +192,9 @@ const DescriptionWrapper = styled.div`
 const QuickView = (): JSX.Element => {
   const [display, setDisplay] = useState<boolean>(true);
 
+
   return(
-    <WrapperOuter display={display}>
+    <WrapperOuter status={display}>
       <WrapperInner>
         <TitleWrapper>
           <Title>QUICK VIEW</Title>
@@ -101,9 +207,52 @@ const QuickView = (): JSX.Element => {
           </Close>
         </TitleWrapper>
 
+
         <img src={quickViewImage} alt='woman in white dress' />
 
+
         <DescriptionWrapper>
+          <Index>MU-4587-89</Index>
+          <Name>DETAILED SWING DRESS</Name>
+          <Details>Full Product Details</Details>
+          <Price>$1,875.00</Price>
+
+          <Select attr='size' defaultValue='default'>
+            <option value='default' disabled>size</option>
+            <option value='8'>8</option>
+            <option value='12'>12</option>
+            <option value='16'>16</option>
+          </Select>
+
+          <Select attr ='color' defaultValue='default'>
+            <option value='default' disabled>color</option>
+            <option value='white'>white</option>
+            <option value='yellow'>yellow</option>
+            <option value='blue'>blue</option>
+          </Select>
+
+          <Select attr='qty' defaultValue='default'>
+            <option value='default' disabled>qty</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+          </Select>
+
+          <ButtonsWrapper>
+            <AddToCart type='button'>
+              ADD TO CART
+            </AddToCart>
+
+            <WishList type='button'>
+              <img src={wishList} alt='wish list' />
+              WISHLIST
+            </WishList>
+
+            <Compare type='button'>
+              <img src={compare} alt='compare' />
+              COMPARE
+            </Compare>
+          </ButtonsWrapper>
         </DescriptionWrapper>
       </WrapperInner>
     </WrapperOuter>
