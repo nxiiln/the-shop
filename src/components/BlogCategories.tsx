3465367aa4770bd2c8f1516d79b2322185c343c8
nxiiln <barbashin.nv@gmail.com>
2022-03-25 08:08:19 +0300

    BlogCategories component created

---
+++ src/components/BlogCategories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+
+const Categories = styled.article`
+  width: 276px;
+  height: 201px;
+  position: absolute;
+  top: 60px;
+  left: 71.3%;
+  border: 1px solid #e4e2e1;
+`;
+
+
+const BlogCategories = (): JSX.Element => {
+	return(
+    <Categories>
+    </Categories>
+  );
+}
+
+
+export default BlogCategories;
