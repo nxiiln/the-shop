b818a75ec3a70020fef0106470b79770d4e94bf2
nxiiln <barbashin.nv@gmail.com>
2022-03-25 08:08:55 +0300

    BlogLabels component created

---
+++ src/components/BlogLabels.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+
+const Labels = styled.article`
+  width: 276px;
+  height: 609px;
+  position: absolute;
+  top: 280px;
+  left: 71.3%;
+  border: 1px solid #e4e2e1;
+`;
+
+
+const BlogLabels = (): JSX.Element => {
+  return(
+    <Labels>
+    </Labels>
+  );
+}
+
+
+export default BlogLabels;
