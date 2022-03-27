b90fb118b623d6f227d3b5f48d8b4e58ed513a78
nxiiln <barbashin.nv@gmail.com>
2022-03-27 11:46:26 +0300

    BlogPost component created

---
+++ src/components/BlogPost.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import blogPost2 from '../images/blogPost2.png';
+
+
+
+
+const WrapperOuter = styled.main`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const WrapperInner = styled.div`
+  width: 960px;
+  height: 2075px;
+  position: relative;
+  border: 1px solid orange;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-bottom: 10px;
+  background: #aaa;
+`;
+
+const BreadCrumbsWrapper = styled.article`
+  width: 100%;
+  margin-bottom: 35px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BreadCrumbs = styled.button`
+  font-family: Arial;
+  font-size: 10px;
+  line-height: 1.2;
+  font-weight: 400;
+  color: #aaa;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+const BreadCrumbsLeft = styled(BreadCrumbs)`
+  width: 68px;
+  height: 10px;
+`;
+
+const BreadCrumbsRight = styled(BreadCrumbs)`
+  width: 130px;
+  height: 10px;
+`;
+
+
+
+
+const BlogPost = (): JSX.Element => {
+  return(
+    <WrapperOuter>
+      <WrapperInner>
+        <Line />
+        <BreadCrumbsWrapper>
+          <BreadCrumbsLeft type='button'>
+            Home / Blog
+          </BreadCrumbsLeft>
+          <BreadCrumbsRight type='button'>
+            {'<'} Return to Previous Page
+          </BreadCrumbsRight>
+        </BreadCrumbsWrapper>
+      </WrapperInner>
+    </WrapperOuter>
+  );
+}
+
+
+
+
+export default BlogPost;
