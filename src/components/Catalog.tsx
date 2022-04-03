878b8615109af849f165d8d6856dba074a3876f2
nxiiln <barbashin.nv@gmail.com>
2022-04-03 18:47:07 +0300

    Images added

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,5 +1,16 @@
 import React, {useState} from 'react';
 import styled from 'styled-components/macro';
+import banner from '../images/banner.png';
+import bannerSmall from '../images/bannerSmall.png';
+import product1 from '../images/product1.png';
+import product2 from '../images/product2.png';
+import product3 from '../images/product3.png';
+import product4 from '../images/product4.png';
+import product5 from '../images/product5.png';
+import product6 from '../images/product6.png';
+import product7 from '../images/product7.png';
+import product8 from '../images/product8.png';
+import product9 from '../images/product9.png';
 
 
 
