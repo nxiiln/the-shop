f509c2d013912e16a1114bee80b8cd11a2ce96b6
nxiiln <barbashin.nv@gmail.com>
2022-04-22 10:33:33 +0300

    <BreadCrumbs> added to Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import BreadCrumbs from './BreadCrumbs';
 
 
 const WrapperOuter = styled.main`
@@ -11,41 +12,6 @@ const WrapperOuter = styled.main`
 const WrapperInner = styled.div`
   width: 960px;
   height: 795px;
-`;
-
-const Line = styled.div`
-  width: 100%;
-  height: 1px;
-  margin-bottom: 10px;
-  background: #aaa;
-`;
-
-const BreadCrumbsWrapper = styled.div`
-  width: 100%;
-  margin-bottom: 30px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const BreadCrumbs = styled.button`
-  font-family: Arial;
-  font-size: 10px;
-  line-height: 1.2;
-  font-weight: 400;
-  color: #aaa;
-  background: transparent;
-  border: none;
-  cursor: pointer;
-`;
-
-const BreadCrumbsLeft = styled(BreadCrumbs)`
-  width: 72px;
-  height: 10px;
-`;
-
-const BreadCrumbsRight = styled(BreadCrumbs)`
-  width: 130px;
-  height: 10px;
 `;
 
 const UserLogin = styled.div`
@@ -173,15 +139,18 @@ const Login = (): JSX.Element => {
   return(
     <WrapperOuter>
       <WrapperInner>
-        <Line />
-        <BreadCrumbsWrapper>
-          <BreadCrumbsLeft type='button'>
-            Home / Login
-          </BreadCrumbsLeft>
-          <BreadCrumbsRight type='button'>
-            {'<'} Return to Previous Page
-          </BreadCrumbsRight>
-        </BreadCrumbsWrapper>
+        <BreadCrumbs
+          link={
+            <>
+              <a href='#'>Home</a>
+              <span>/</span>
+              <span>Create An Account</span>
+            </>
+          }
+          return='#'
+          marginBottom='20px'
+          gridArea=''
+        />
 
         <UserLogin>
           <TitleWrapper>
