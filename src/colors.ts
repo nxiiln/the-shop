214a486235357dfe82b287dd3531ae03617e5a0a
nxiiln <barbashin.nv@gmail.com>
2022-05-02 07:43:33 +0300

    --color-top-dropdown added

--- src/colors.ts
+++ src/colors.ts
@@ -4,12 +4,17 @@ const colors: string = `
     --color-text-second: #fff;
     --color-text-regular: #aaa;
     --color-text-highlight: #aea012;
+
     --color-background-main: #fff;
     --color-background-second: #000;
     --color-background-highlight: #f7f7f7;
+
     --color-border: #e4e2e1;
+
     --color-triangle-new: #000;
     --color-triangle-sale: #c50e20;
+    
+    --color-top-dropdown: #333;
   }
 `;
 
