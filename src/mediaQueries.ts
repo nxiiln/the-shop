8f2d88b9185e4e8bf3e5fff8b097ce6dd0562a51
nxiiln <barbashin.nv@gmail.com>
2022-05-10 16:33:31 +0300

    Fixed - "can't perform a react state update on an unmounted component"

--- src/mediaQueries.ts
+++ src/mediaQueries.ts
@@ -5,7 +5,6 @@ export const bigScreen: string = '(min-width: 961px)';
 export const middleScreen: string = '(min-width: 731px) and (max-width: 960px)';
 export const smallScreen: string = '(max-width: 730px)';
 
-
 interface Screen {
   big: boolean;
   middle: boolean;
@@ -17,20 +16,26 @@ export const useMediaQuery = (): Screen => {
   const [middle, setMiddle] = useState<boolean>(window.matchMedia(middleScreen).matches);
   const [small, setSmall] = useState<boolean>(window.matchMedia(smallScreen).matches);
   const screen: Screen = {big: big, middle: middle, small: small};
+ 
+  useEffect(() => {
+    const queryBigScreen: MediaQueryList = window.matchMedia(bigScreen);
+    const queryMiddleScreen: MediaQueryList = window.matchMedia(middleScreen);
+    const querySmallScreen: MediaQueryList = window.matchMedia(smallScreen);
 
-  useEffect((): void => {
-    window
-      .matchMedia(bigScreen)
-      .addEventListener('change', (e: MediaQueryListEvent): void => setBig(e.matches));
+    const listenerBigScreen = (): void => setBig(queryBigScreen.matches);
+    const listenerMiddleScreen = (): void => setMiddle(queryMiddleScreen.matches);
+    const listenerSmallScreen = (): void => setSmall(querySmallScreen.matches);
 
-    window
-      .matchMedia(middleScreen)
-      .addEventListener('change', (e: MediaQueryListEvent): void => setMiddle(e.matches));
+    queryBigScreen.addEventListener('change', listenerBigScreen);
+    queryMiddleScreen.addEventListener('change', listenerMiddleScreen);
+    querySmallScreen.addEventListener('change', listenerSmallScreen);
 
-    window
-      .matchMedia(smallScreen)
-      .addEventListener('change', (e: MediaQueryListEvent): void => setSmall(e.matches));
-  });
+    return (): void => {
+      queryBigScreen.removeEventListener('change', listenerBigScreen);
+      queryMiddleScreen.removeEventListener('change', listenerMiddleScreen);
+      querySmallScreen.removeEventListener('change', listenerSmallScreen);
+    }
+  }, [big, middle, small]);
 
   return screen;
 }
