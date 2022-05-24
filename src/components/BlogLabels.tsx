b38fcf9c57a313d4c0be09b2896e6090e84cd88c
nxiiln <barbashin.nv@gmail.com>
2022-05-24 13:47:25 +0300

    <BlogLabels> adapted for big screen

--- src/components/BlogLabels.tsx
+++ src/components/BlogLabels.tsx
@@ -1,97 +1,103 @@
-import React from 'react';
 import styled from 'styled-components/macro';
 
 
-const BlogLabelsWrapper = styled.article`
-  width: 276px;
-  height: 609px;
-  position: absolute;
-  top: 280px;
-  left: 71.3%;
-  border: 1px solid #e4e2e1;
+const Wrapper = styled.article<{gridArea: string}>`
+  width: 275px;
+  height: max-content;
+  padding-bottom: 20px;
+  grid-area: ${props => props.gridArea};
+  justify-self: end;
+  border: 1px solid var(--color-border);
 `;
 
-const Labels = styled.h2`
+const Title = styled.h2`
   margin: 20px 0 0 20px;
   font-family: var(--font-second);
   font-size: 14px;
   line-height: 1.2;
   font-weight: 300;
-  color: #000;
+  color: var(--color-text-main);
 `;
 
 const LabelsWrapper = styled.div`
-  width: 239px;
-  height: 525px;
+  width: 240px;
   margin: 20px 0 0 20px;
 `;
 
-const Label = styled.span<{size: string}>`
-  font-family: Tahoma;
-  font-size: ${props => props.size};
+const Label = styled.span<{fontSize: string}>`
+  font-family: var(--font-regular);
+  font-size: ${props => props.fontSize};
   line-height: 24px;
   font-weight: 400;
-  color: #000;
+  color: var(--color-text-main);
+  cursor: pointer;
+  &:hover {text-decoration: underline}
 `;
 
 
-const BlogLabels = (): JSX.Element => {
+const labels: {fontSize: string, label: string}[] = [
+  {fontSize: '10px', label: 'ALAÏA ALEXA '},
+  {fontSize: '14px', label: 'CHUNG ALEXANDER '},
+  {fontSize: '10px', label: 'WANG BAGS '},
+  {fontSize: '18px', label: 'BALENCIAGA '},
+  {fontSize: '10px', label: 'BASICS BEAUTY BELTS '},
+  {fontSize: '12px', label: 'BLOGGERS BOOTS '},
+  {fontSize: '10px', label: 'BRACELETS '},
+  {fontSize: '18px', label: 'BRIGHTS BURBERRY '},
+  {fontSize: '10px', label: 'BUTTON CAROLINE ISSA '},
+  {fontSize: '14px', label: 'CAROLINE '},
+  {fontSize: '14px', label: 'CLASSIC CLUTCH '},
+  {fontSize: '10px', label: 'CÉLINE '},
+  {fontSize: '10px', label: 'DARIA "DASHA" ZHUKOVA '},
+  {fontSize: '10px', label: 'DIANE KRUGER DRESS '},
+  {fontSize: '14px', label: 'EARRINGS EDITOR '},
+  {fontSize: '10px', label: 'STYLE EDITORIALS '},
+  {fontSize: '10px', label: 'ELIZABETH + '},
+  {fontSize: '10px', label: 'JAMES FLORAL '},
+  {fontSize: '10px', label: 'FUR GAIA '},
+  {fontSize: '24px', label: 'REPOSSI GIORGIA '},
+  {fontSize: '10px', label: 'TORDINI GIOVANNA BATTA-GLIA '},
+  {fontSize: '12px', label: 'GIUSEPPE ZANOTTI '},
+  {fontSize: '10px', label: 'GIVENCHY '},
+  {fontSize: '10px', label: 'GUCCI '},
+  {fontSize: '10px', label: 'GUINEVERE VAN '},
+  {fontSize: '14px', label: 'SEENUS '},
+  {fontSize: '10px', label: 'GWEN STEFANI '},
+  {fontSize: '14px', label: 'GÉRALDINE SAGLIO '},
+  {fontSize: '10px', label: 'HATS HEELS HOME '},
+  {fontSize: '10px', label: 'DESIGN INSPIRATION '},
+  {fontSize: '14px', label: 'ISABEL '},
+  {fontSize: '10px', label: 'MARANT JACKET/COAT KATE MOSS '},
+  {fontSize: '10px', label: 'KATE MOSS '},
+  {fontSize: '12px', label: 'ARKUS LUPFER '},
+  {fontSize: '10px', label: 'METALLICS MIRANDA '},
+  {fontSize: '18px', label: 'KERR '},
+  {fontSize: '10px', label: 'MIROSLAV '},
+  {fontSize: '10px', label: 'PLAT '},
+  {fontSize: '24px', label: 'SCHOULER '},
+  {fontSize: '10px', label: 'PYTHON RINGS '},
+  {fontSize: '10px', label: 'SNEAKERS '},
+  {fontSize: '10px', label: 'STREET STYLE '},
+  {fontSize: '14px', label: 'SUNGLASSES SWEATER '},
+  {fontSize: '10px', label: 'TEXTILE ELIZABETH '},
+  {fontSize: '12px', label: '+ JAMES '},
+  {fontSize: '10px', label: 'THE ROW '},
+  {fontSize: '12px', label: 'THEORY '},
+  {fontSize: '10px', label: 'THEYSKENS THEORY '},
+  {fontSize: '10px', label: 'TOPS VEST '},
+];
+
+
+const BlogLabels = ({gridArea}: {gridArea: string}): JSX.Element => {
   return(
-    <BlogLabelsWrapper>
-      <Labels>LABELS</Labels>
+    <Wrapper gridArea={gridArea}>
+      <Title>LABELS</Title>
       <LabelsWrapper>
-        <Label size='10px'>ALAÏA ALEXA </Label>
-        <Label size='14px'>CHUNG ALEXANDER </Label>
-        <Label size='10px'>WANG BAGS </Label>
-        <Label size='18px'>BALENCIAGA </Label>
-        <Label size='10px'>BASICS BEAUTY BELTS </Label>
-        <Label size='12px'>BLOGGERS BOOTS </Label>
-        <Label size='10px'>BRACELETS </Label>
-        <Label size='18px'>BRIGHTS BURBERRY </Label>
-        <Label size='10px'>BUTTON CAROLINE ISSA </Label>
-        <Label size='14px'>CAROLINE </Label>
-        <Label size='10px'>CAROLINE </Label>
-        <Label size='14px'>CLASSIC CLUTCH </Label>
-        <Label size='10px'>CÉLINE </Label>
-        <Label size='10px'>DARIA "DASHA" ZHUKOVA </Label>
-        <Label size='10px'>DIANE KRUGER DRESS </Label>
-        <Label size='14px'>EARRINGS EDITOR </Label>
-        <Label size='10px'>STYLE EDITORIALS </Label>
-        <Label size='10px'>ELIZABETH + </Label>
-        <Label size='10px'>JAMES FLORAL </Label>
-        <Label size='10px'>FUR GAIA </Label>
-        <Label size='24px'>REPOSSI GIORGIA </Label>
-        <Label size='10px'>TORDINI GIOVANNA BATTA-GLIA </Label>
-        <Label size='12px'>GIUSEPPE ZANOTTI </Label>
-        <Label size='10px'>GIVENCHY </Label>
-        <Label size='10px'>GUCCI </Label>
-        <Label size='10px'>GUINEVERE VAN </Label>
-        <Label size='14px'>SEENUS </Label>
-        <Label size='10px'>GWEN STEFANI </Label>
-        <Label size='14px'>GÉRALDINE SAGLIO </Label>
-        <Label size='10px'>HATS HEELS HOME </Label>
-        <Label size='10px'>DESIGN INSPIRATION </Label>
-        <Label size='14px'>ISABEL </Label>
-        <Label size='10px'>MARANT JACKET/COAT KATE MOSS </Label>
-        <Label size='10px'>KATE MOSS </Label>
-        <Label size='12px'>ARKUS LUPFER </Label>
-        <Label size='10px'>METALLICS MIRANDA </Label>
-        <Label size='18px'>KERR </Label>
-        <Label size='10px'>MIROSLAV </Label>
-        <Label size='10px'>PLAT </Label>
-        <Label size='24px'>SCHOULER </Label>
-        <Label size='10px'>PYTHON RINGS </Label>
-        <Label size='10px'>SNEAKERS </Label>
-        <Label size='10px'>STREET STYLE </Label>
-        <Label size='14px'>SUNGLASSES SWEATER </Label>
-        <Label size='10px'>TEXTILE ELIZABETH </Label>
-        <Label size='12px'>+ JAMES </Label>
-        <Label size='10px'>THE ROW </Label>
-        <Label size='12px'>THEORY </Label>
-        <Label size='10px'>THEYSKENS' THEORY </Label>
-        <Label size='10px'>TOPS VEST </Label>
+        {labels.map((label: {fontSize: string, label: string}): JSX.Element => {
+          return <Label key={label.label} fontSize={label.fontSize}>{label.label}</Label>;
+        })}
       </LabelsWrapper>
-    </BlogLabelsWrapper>
+    </Wrapper>
   );
 }
 
