0fb3a11109be03a7e1392a49a9ca1ff6ba03c8be
nxiiln <barbashin.nv@gmail.com>
2022-06-16 23:40:56 +0300

    General refactoring

--- src/components/WishList.tsx
+++ src/components/WishList.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import styled from 'styled-components/macro';
 import BreadCrumbs from './BreadCrumbs';
 import cartProductA from '../images/cartProductA.png';
@@ -23,10 +23,9 @@ const WrapperInner = styled.div`
 
 const CartWrapper = styled.div`
   grid-area: 3 / 1 / 4 / 1;
-  border: 1px solid #e4e2e1;
-  div:last-child {
-    margin-bottom: 0;
-  }
+  border: 1px solid var(--color-border);
+
+  > div:last-child {margin-bottom: 0}
 `;
 
 const HeaderWrapper = styled.div`
@@ -34,13 +33,14 @@ const HeaderWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  border-bottom: 1px solid #e4e2e1;
-  h2 {
+  border-bottom: 1px solid var(--color-border);
+
+  > h2 {
     font-family: var(--font-main);
     font-size: 24px;
     line-height: 1.2;
     font-weight: 400;
-    color: #000;
+    color: var(--color-text-main);
   }
 `;
 
@@ -54,16 +54,18 @@ const TitleWrapper = styled.div`
     minmax(40px, 10.2%) minmax(30px, 31.6%) minmax(119px, 1fr);
   justify-items: start;
   align-items: center;
-  span {
+
+  > span {
     font-family: var(--font-second);
     font-size: 12px;
     line-height: 1.2;
     font-weight: 300;
-    color: #000;
+    color: var(--color-text-main);
   }
-  span:nth-child(1) {grid-area: 1 / 2 / 1 / 3;}
-  span:nth-child(2) {grid-area: 1 / 4 / 1 / 5;}
-  span:nth-child(3) {grid-area: 1 / 5 / 1 / 6;}
+
+  > span:nth-child(1) {grid-area: 1 / 2 / 1 / 3}
+  > span:nth-child(2) {grid-area: 1 / 4 / 1 / 5}
+  > span:nth-child(3) {grid-area: 1 / 5 / 1 / 6}
 `;
 
 const ProductWrapper = styled.div`
@@ -73,7 +75,7 @@ const ProductWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #f7f7f7;
+  background: var(--color-background-highlight);
 `;
 
 const Product = styled.div`
@@ -81,15 +83,15 @@ const Product = styled.div`
   height: 120px;
   display: grid;
 
-  button:nth-child(1) {grid-area: 2 / 1 / 3 / 2;}
-  img:nth-child(2) {grid-area: 1 / 2 / 13 / 3;}
-  span:nth-child(3) {grid-area: 2 / 3 / 3 / 4;}
-  span:nth-child(4) {grid-area: 4 / 3 / 5 / 4;}
-  span:nth-child(5) {grid-area: 6 / 3 / 7 / 4;}
-  button:nth-child(6) {grid-area: 11 / 3 / 12 / 4;}
-  span:nth-child(7) {grid-area: 2 / 4 / 3 / 5;}
-  span:nth-child(8) {grid-area: 2 / 5 / 3 / 6;}
-  button:nth-child(9) {grid-area: 2 / 6 / 5 / 7;}
+  > button:nth-child(1) {grid-area: 2 / 1 / 3 / 2}
+  > img:nth-child(2) {grid-area: 1 / 2 / 13 / 3}
+  > span:nth-child(3) {grid-area: 2 / 3 / 3 / 4}
+  > span:nth-child(4) {grid-area: 4 / 3 / 5 / 4}
+  > span:nth-child(5) {grid-area: 6 / 3 / 7 / 4}
+  > button:nth-child(6) {grid-area: 11 / 3 / 12 / 4}
+  > span:nth-child(7) {grid-area: 2 / 4 / 3 / 5}
+  > span:nth-child(8) {grid-area: 2 / 5 / 3 / 6}
+  > button:nth-child(9) {grid-area: 2 / 6 / 5 / 7}
 
   grid-template-columns:
     minmax(25px, 4.8%) minmax(83px, 12%) minmax(150px, 23%)
@@ -99,13 +101,14 @@ const Product = styled.div`
   justify-items: start;
   align-items: center;
   background: transparent;
-  span {
+  
+  > span {
     font-family: var(--font-second);
     font-size: 10px;
     line-height: 14px;
     font-weight: 400;
     text-transform: uppercase;
-    color: #000;
+    color: var(--color-text-main);
   }
 `;
 
@@ -116,7 +119,7 @@ const ButtonUnderline = styled.button`
   line-height: 1.2;
   font-weight: 400;
   text-decoration: underline;
-  color: #000;
+  color: var(--color-text-main);
   background: transparent;
   border: none;
   cursor: pointer;
@@ -135,17 +138,6 @@ const X = styled.button`
   cursor: pointer;
 `;
 
-const Quantity = styled.span`
-  font-family: var(--font-second);
-  font-size: 10px;
-  line-height: 1.2;
-  font-weight: 400;
-  color: #000;
-  background: #fff;
-  border: 1px solid #e4e2e1;
-}
-`;
-
 const AddToBag = styled.button`
   width: 119px;
   height: 30px;
@@ -153,8 +145,8 @@ const AddToBag = styled.button`
   font-size: 10px;
   line-height: 1.2;
   font-weight: 300;
-  color: #fff;
-  background: #000;
+  color: var(--color-text-second);
+  background: var(--color-background-second);
   border: none;
   cursor: pointer;
 `;
@@ -162,43 +154,45 @@ const AddToBag = styled.button`
 
 
 
-const WishList = (): JSX.Element => {
-  interface Product {
-    id: number;
-    status: boolean;
-    image: string;
-    name: string;
-    color: string;
-    size: number;
-    price: number;
-    quantity: number;
+interface Product {
+  id: number;
+  status: boolean;
+  image: string;
+  name: string;
+  color: string;
+  size: number;
+  price: number;
+  quantity: number;
+}
+
+const productList: Product[] = [
+  {
+    id: 1,
+    status: true,
+    image: cartProductA,
+    name: 'detailed swing dress',
+    color: 'yellow',
+    size: 12,
+    price: 275,
+    quantity: 1
+  },
+  {
+    id: 2,
+    status: true,
+    image: cartProductB,
+    name: 'maxararzy frilled dress',
+    color: 'blue',
+    size: 14,
+    price: 325,
+    quantity: 2
   }
+];
 
-  const productList: Product[] = [
-    {
-      id: 1,
-      status: true,
-      image: cartProductA,
-      name: 'detailed swing dress',
-      color: 'yellow',
-      size: 12,
-      price: 275,
-      quantity: 1
-    },
-    {
-      id: 2,
-      status: true,
-      image: cartProductB,
-      name: 'maxararzy frilled dress',
-      color: 'blue',
-      size: 14,
-      price: 325,
-      quantity: 2
-    }
-  ];
 
+
+
+const WishList = (): JSX.Element => {
   const [products, setProducts] = useState<Product[]>(productList);
-
 
   return(
     <WrapperOuter>
@@ -213,7 +207,6 @@ const WishList = (): JSX.Element => {
           }
           return='#'
           marginBottom='20px'
-          gridArea=''
         />
 
         <CartWrapper>
@@ -226,8 +219,8 @@ const WishList = (): JSX.Element => {
             <span>Q-TY</span>
           </TitleWrapper>
 
-          {products.map((product: Product): false | JSX.Element => {
-            return(product.status &&
+          {products.map((product: Product): false | JSX.Element =>
+            product.status &&
               <ProductWrapper key={product.id}>
                 <Product>
                   <X
@@ -251,8 +244,7 @@ const WishList = (): JSX.Element => {
                   <AddToBag type='button'>ADD TO BAG</AddToBag>
                 </Product>
               </ProductWrapper>
-            );
-          })}
+          )}
         </CartWrapper>
       </WrapperInner>
     </WrapperOuter>
