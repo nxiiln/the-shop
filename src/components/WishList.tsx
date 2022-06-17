05ddc65536731a4be1299f6d802abbd2a70ac5cb
nxiiln <barbashin.nv@gmail.com>
2022-06-17 06:22:59 +0300

    <WishList> adapted for small screen

--- src/components/WishList.tsx
+++ src/components/WishList.tsx
@@ -3,20 +3,19 @@ import styled from 'styled-components/macro';
 import BreadCrumbs from './BreadCrumbs';
 import cartProductA from '../images/cartProductA.png';
 import cartProductB from '../images/cartProductB.png';
+import {smallScreen, useMediaQuery} from '../mediaQueries';
 
 
 
 
-const WrapperOuter = styled.section`
+const WrapperOuter = styled.main`
   width: 100vw;
-  margin: 0 0 370px 0;
   display: flex;
   justify-content: center;
 `;
 
 const WrapperInner = styled.div`
   width: 1100px;
-  min-width: 760px;
   padding: 0 1% 50px;
   display: grid;
   grid-template-columns: 1fr;
@@ -78,6 +77,8 @@ const ProductWrapper = styled.div`
   justify-content: center;
   align-items: center;
   background: var(--color-background-highlight);
+
+  @media ${smallScreen} {height: 245px}
 `;
 
 const Product = styled.div`
@@ -96,10 +97,10 @@ const Product = styled.div`
   > button:nth-child(9) {grid-area: 2 / 6 / 5 / 7}
 
   grid-template-columns:
-    minmax(25px, 4.8%) minmax(83px, 12%) minmax(150px, 23%)
+    minmax(25px, 4.8%) minmax(90px, 12%) minmax(150px, 23%)
     minmax(40px, 10.2%) minmax(30px, 31.6%) minmax(119px, 1fr);
 
-  grid-template-rows: repeat(12, 10px);
+  grid-template-rows: repeat(12, 8.3%);
   justify-items: start;
   align-items: center;
   background: transparent;
@@ -111,6 +112,34 @@ const Product = styled.div`
     font-weight: 400;
     text-transform: uppercase;
     color: var(--color-text-main);
+  }
+
+  @media ${smallScreen} {
+    width: 300px;
+    height: 210px;
+    grid-template-columns: 10% 40% 50%;
+
+    > button:nth-child(1) {
+      grid-area: 2 / 1 / 3 / 2;
+      justify-self: center;
+    }
+
+    > img:nth-child(2) {
+      grid-area: 2 / 2 / 9 / 3;
+      justify-self: center;
+    }
+
+    > span:nth-child(3) {grid-area: 2 / 3 / 3 / 4}
+    > span:nth-child(4) {grid-area: 3 / 3 / 4 / 4}
+    > span:nth-child(5) {grid-area: 4 / 3 / 5 / 4}
+    > button:nth-child(6) {grid-area: 9 / 3 / 10 / 4}
+    > span:nth-child(7) {grid-area: 5 / 3 / 7 / 4}
+    > span:nth-child(8) {grid-area: 7 / 3 / 9 / 4}
+
+    > button:nth-child(9) {
+      grid-area: 11 / 2 / 13 / 4;
+      justify-self: center;
+    }
   }
 `;
 
@@ -195,6 +224,8 @@ const productList: Product[] = [
 
 const WishList = (): JSX.Element => {
   const [products, setProducts] = useState<Product[]>(productList);
+  const screen = useMediaQuery();
+
 
   return(
     <WrapperOuter>
@@ -204,7 +235,7 @@ const WishList = (): JSX.Element => {
             <>
               <a href='#'>Home</a>
               <span>/</span>
-              <span>Create An Account</span>
+              <span>Wish List</span>
             </>
           }
           return='#'
@@ -215,11 +246,14 @@ const WishList = (): JSX.Element => {
           <HeaderWrapper>
             <h2>WISH LIST</h2>
           </HeaderWrapper>
-          <TitleWrapper>
-            <span>PRODUCT</span>
-            <span>PRICE</span>
-            <span>Q-TY</span>
-          </TitleWrapper>
+
+          {!screen.small &&
+            <TitleWrapper>
+              <span>PRODUCT</span>
+              <span>PRICE</span>
+              <span>Q-TY</span>
+            </TitleWrapper>
+          }
 
           {products.map((product: Product): false | JSX.Element =>
             product.status &&
@@ -242,7 +276,7 @@ const WishList = (): JSX.Element => {
                   <span>size: {product.size}</span>
                   <ButtonUnderline type='button'>Edit Item</ButtonUnderline>
                   <span>${product.price}</span>
-                  <span>{product.quantity}</span>
+                  <span>{screen.small && 'Q-ty: '}{product.quantity}</span>
                   <AddToBag type='button'>ADD TO BAG</AddToBag>
                 </Product>
               </ProductWrapper>
