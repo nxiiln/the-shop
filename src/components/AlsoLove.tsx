34813675a68c6d6d36bbd960324640887a45a3a6
nxiiln <barbashin.nv@gmail.com>
2022-06-21 19:53:41 +0300

    General refactoring

--- src/components/AlsoLove.tsx
+++ src/components/AlsoLove.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import styled from 'styled-components/macro';
 import QuickView from './QuickView';
 import alsoLove1 from '../images/alsoLove1.png';
@@ -14,8 +14,9 @@ const AlsoLoveWrapper = styled.article<{status: boolean}>`
   height: 296px;
   margin-bottom: 30px;
   position: relative;
-  background-color: #f7f7f7;
-  border: 1px solid #e4e2e1;
+  background: var(--color-background-highlight);
+  border: 1px solid var(--color-border);
+
   > span:first-child {
     position: relative;
     top: 25px;
@@ -23,7 +24,7 @@ const AlsoLoveWrapper = styled.article<{status: boolean}>`
     font-family: var(--font-main);
     font-size: 21px;
     font-weight: 400;
-    color: #000;
+    color: var(--color-text-main);
   }
 `;
 
@@ -33,9 +34,10 @@ const AlsoLoveClose = styled.button`
   position: absolute;
   top: 10px;
   right: 10px;
-  background: #fff;
-  border: 1px solid #e4e2e1;
+  background: var(--color-background-main);
+  border: 1px solid var(--color-border);
   cursor: pointer;
+
   > span:first-child  {
     display: inline-block;
     position: absolute;
@@ -43,7 +45,7 @@ const AlsoLoveClose = styled.button`
     left: 7px;
     font-family: var(--font-second);
     font-size: 25px;
-    color: #000;
+    color: var(--color-text-main);
     transform: rotate(45deg);
   }
 `;
@@ -64,6 +66,7 @@ const ProductAlso = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
+
   > span:nth-child(2) {
     width: 115px;
     margin: 0 0 20px 10px;
@@ -72,15 +75,16 @@ const ProductAlso = styled.div`
     line-height: 18px;
     font-weight: 400;
     text-transform: uppercase;
-    color: #000;
+    color: var(--color-text-main);
   }
+
   > span:nth-child(3) {
     margin: 0 0 25px 10px;
     font-family: var(--font-second);
     font-size: 12px;
     line-height: 1.2;
     font-weight: 300;
-    color: #000;
+    color: var(--color-text-main);
   }
 `;
 
@@ -90,45 +94,47 @@ const ButtonQuickView = styled.button`
   margin-left: 10px;
   font-family: var(--font-second);
   font-size: 10px;
-  color: #000;
-  background: #fff;
-  border: 1px solid #e4e2e1;
+  color: var(--color-text-main);
+  background: var(--color-background-main);
+  border: 1px solid var(--color-border);
   cursor: pointer;
 `;
 
 
 
 
-const AlsoLove = (): JSX.Element => {
-  interface ProductAlso {
-    id: number;
-    image: string;
-    name: string;
-    price: number;
+interface ProductAlso {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+}
+
+const productsAlso: ProductAlso[] = [
+  {
+    id: 1,
+    image: alsoLove1,
+    name: 'detailed swing dress',
+    price: 1875
+  },
+  {
+    id: 2,
+    image: alsoLove2,
+    name: 'detailed swing dress',
+    price: 850
+  },
+  {
+    id: 3,
+    image: alsoLove3,
+    name: 'detailed swing dress',
+    price: 875
   }
-
-  const productsAlso: ProductAlso[] = [
-    {
-      id: 1,
-      image: alsoLove1,
-      name: 'detailed swing dress',
-      price: 1875
-    },
-    {
-      id: 2,
-      image: alsoLove2,
-      name: 'detailed swing dress',
-      price: 850
-    },
-    {
-      id: 3,
-      image: alsoLove3,
-      name: 'detailed swing dress',
-      price: 875
-    }
-  ];
+];
 
 
+
+
+const AlsoLove = (): JSX.Element => {
   const [display, setDisplay] = useState<boolean>(true);
   const [quickView, setQuickView] = useState<boolean>(false);
 
@@ -136,31 +142,32 @@ const AlsoLove = (): JSX.Element => {
   return(
     <AlsoLoveWrapper status={display}>
       <span>YOU WILL ALSO LOVE</span>
+      
       <AlsoLoveClose
         type='button'
         onClick={(): void => setDisplay(false)}
       >
         <span>+</span>
       </AlsoLoveClose>
+
       <ProductAlsoWrapper>
-        {productsAlso.map((product: ProductAlso): JSX.Element => {
-          return(
-            <ProductAlso key={product.id}>
-              <img src={product.image} alt={product.name} />
-              <span>{product.name}</span>
-              <span>
-                ${product.price.toString().replace(/(.+)(...)$/, '$1,$2')}
-              </span>
-              <ButtonQuickView
-                type='button'
-                onClick={(): void => setQuickView(true)}
-              >
-                QUICKVIEW
-              </ButtonQuickView>
-            </ProductAlso>
-          )
-        })}
+        {productsAlso.map((product: ProductAlso): JSX.Element =>
+          <ProductAlso key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <span>{product.name}</span>
+            <span>
+              ${product.price.toString().replace(/(.+)(...)$/, '$1,$2')}
+            </span>
+            <ButtonQuickView
+              type='button'
+              onClick={(): void => setQuickView(true)}
+            >
+              QUICKVIEW
+            </ButtonQuickView>
+          </ProductAlso>
+        )}
       </ProductAlsoWrapper>
+
       {quickView && <QuickView />}
     </AlsoLoveWrapper>
   )
