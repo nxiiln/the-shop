3e6f79d74c9b0dfe7eada9295822fc9eaa783e08
nxiiln <barbashin.nv@gmail.com>
2022-06-22 15:58:46 +0300

    General refactoring of the <BreadCrumbs>

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components/macro';
 
 
@@ -28,36 +27,27 @@ const BreadCrumbsLeft = styled.div`
   color: var(--color-text-regular);
   user-select: none;
 
-  a {
+  > a {
     margin-right: 5px;
     font-family: var(--font-regular);
     font-size: 10px;
     font-weight: 400;
     color: var(--color-text-regular);
     text-decoration: none;
-    :hover {
-      text-decoration: underline;
-    }
+    
+    &:hover {text-decoration: underline}
   }
 
-  span {
-    margin-right: 5px;
-  }
+  > span {margin-right: 5px}
 `;
 
 const BreadCrumbsRight = styled(BreadCrumbsLeft)`
-  :hover {
-    text-decoration: underline;
-  }
+  &:hover {text-decoration: underline}
 
-  a {
-    margin: 0;
-  }
+  > a {margin: 0}
 
-  span {
-    :hover {
-      cursor: pointer;
-    }
+  > span {
+    &:hover {cursor: pointer}
   }
 `;
 
@@ -69,22 +59,20 @@ interface Props {
   gridArea?: string;
 }
 
-const BreadCrumbs = (props: Props): JSX.Element => {
-  return(
-    <Wrapper gridArea={props.gridArea === undefined ? '' : props.gridArea}>
-      <Line />
-      <BreadCrumbsWrapper
-        marginBottom={props.marginBottom === undefined ? '' : props.marginBottom}
-      >
-        <BreadCrumbsLeft>{props.link}</BreadCrumbsLeft>
-        <BreadCrumbsRight>
-          <span>&#10094;</span>
-          <a href={props.return}>Return to Previous Page</a>
-        </BreadCrumbsRight>
-      </BreadCrumbsWrapper>
-    </Wrapper>
-  );
-}
+const BreadCrumbs = (props: Props): JSX.Element => (
+  <Wrapper gridArea={props.gridArea === undefined ? '' : props.gridArea}>
+    <Line />
+    <BreadCrumbsWrapper
+      marginBottom={props.marginBottom === undefined ? '' : props.marginBottom}
+    >
+      <BreadCrumbsLeft>{props.link}</BreadCrumbsLeft>
+      <BreadCrumbsRight>
+        <span>❮</span>
+        <a href={props.return}>Return to Previous Page</a>
+      </BreadCrumbsRight>
+    </BreadCrumbsWrapper>
+  </Wrapper>
+);
 
 
 export default BreadCrumbs;
