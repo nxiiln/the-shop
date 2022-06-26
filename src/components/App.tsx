cd303acdd0dfe5cbc1930cedd7864d862139214a
nxiiln <barbashin.nv@gmail.com>
2022-06-26 22:47:31 +0300

    Added routes for <Top>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import Header from './Header';
-import Product from './Product';
 import TopBrand from './TopBrands';
 import PurchasesInfo from './PurchasesInfo';
 import Footer from './Footer';
+import Home from './Home';
+import MyAccount from './MyAccount';
+import WishList from './WishList';
+import Checkout from './Checkout';
+import Login from './Login';
 
 
-const App = (): JSX.Element => {
-  return(
-    <>
-      <Header />
-      <Product />
-      <TopBrand />
-      <PurchasesInfo />
-      <Footer />
-    </>
-  );
-}
+const App = (): JSX.Element => (
+  <>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='my-account' element={<MyAccount />} />
+      <Route path='wish-list' element={<WishList />} />
+      <Route path='checkout' element={<Checkout />} />
+      <Route path='login' element={<Login />} />
+    </Routes>
+    <TopBrand />
+    <PurchasesInfo />
+    <Footer />
+  </>
+);
 
 
 export default App;
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-import CartPreview from './CartPreview';
-import {mediumScreen, useMediaQuery} from '../mediaQueries';
+import {useState} from 'react';
 import styled from 'styled-components/macro';
+import {mediumScreen, useMediaQuery} from '../mediaQueries';
+import {Link} from 'react-router-dom';
+import CartPreview from './CartPreview';
 
 
 
@@ -67,7 +68,7 @@ const Dropdown = styled.div<{open: boolean, width: string, height: string}>`
   `}
 `;
 
-const Link = styled.a`
+const LinkSimple = styled.a`
   width: 65px;
   margin: 11px 0 4px 0;
   font-family: var(--font-second);
@@ -79,7 +80,7 @@ const Link = styled.a`
   &:hover {color: var(--color-text-second)}
 `;
 
-const LinkMain = styled(Link)<{open: boolean}>`
+const LinkMain = styled(LinkSimple)<{open: boolean}>`
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -93,7 +94,7 @@ const LinkMain = styled(Link)<{open: boolean}>`
   }
 `;
 
-const LinkUnderline = styled(Link)<{withoutBorder?: boolean}>`
+const LinkUnderline = styled(LinkSimple)<{withoutBorder?: boolean}>`
   width: auto;
   margin: 0;
   padding: 0 12px 0 12px;
@@ -144,7 +145,7 @@ const Top = (): JSX.Element => {
   return(
     <WrapperOuter>
       <Title>
-        {screen.small || <span>FREE SHIPPING ON ORDERS ABOVE 50$</span>}
+        {!screen.small && <span>FREE SHIPPING ON ORDERS ABOVE 50$</span>}
       </Title>
 
 
@@ -167,8 +168,8 @@ const Top = (): JSX.Element => {
 
               {contactOpen &&
                 <>
-                  <Link href='#store-location'>STORE LOCATION</Link>
-                  <Link href='#about-us'>ABOUT US</Link>
+                  <LinkSimple href='#store-location'>STORE LOCATION</LinkSimple>
+                  <LinkSimple href='#about-us'>ABOUT US</LinkSimple>
                 </>
               }
             </Dropdown>
@@ -204,10 +205,10 @@ const Top = (): JSX.Element => {
 
         {screen.big &&
           <div>
-            <LinkUnderline href='#'>MY ACCOUNT</LinkUnderline>
-            <LinkUnderline href='#'>WISH LIST</LinkUnderline>
-            <LinkUnderline href='#'>CHECKOUT</LinkUnderline>
-            <LinkUnderline href='#' withoutBorder>LOG IN</LinkUnderline>
+            <LinkUnderline as={Link} to='my-account'>MY ACCOUNT</LinkUnderline>
+            <LinkUnderline as={Link} to='wish-list'>WISH LIST</LinkUnderline>
+            <LinkUnderline as={Link} to='checkout'>CHECKOUT</LinkUnderline>
+            <LinkUnderline as={Link} to='login' withoutBorder>LOG IN</LinkUnderline>
           </div>
         }
 
