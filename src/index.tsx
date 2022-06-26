2443c998fd3404ac857279ac7a4534c89e00b27d
nxiiln <barbashin.nv@gmail.com>
2022-06-26 22:11:51 +0300

    Added <BrowserRouter>

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './components/App';
 import GlobalStyles from './globalStyles';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     <GlobalStyles />
-    <App />
   </React.StrictMode>,
   document.querySelector('#root')
 );
