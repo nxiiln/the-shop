2da302c63b739d9b387b4e2e614ae30393963f27
nxiiln <barbashin.nv@gmail.com>
2022-07-09 08:35:04 +0300

    cartSlice added to cart

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
+import cartReducer from './slices/cart';
 
 
-export default configureStore({
-  reducer: {}
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  }
 });
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
