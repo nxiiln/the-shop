7053b23d87b55ed5b8806f5e8bb9d5a6e40de48a
nxiiln <barbashin.nv@gmail.com>
2022-07-14 19:05:05 +0300

    import products

--- src/components/CatalogProducts.tsx
+++ src/components/CatalogProducts.tsx
@@ -3,17 +3,8 @@ import styled from 'styled-components/macro';
 import {HashLink} from 'react-router-hash-link';
 import {useAppSelector, useAppDispatch} from '../redux-hooks';
 import {add} from '../slices/cart';
+import {products} from '../products';
 import {IProduct} from '../IProduct';
-import product1 from '../images/product1.png';
-import product2 from '../images/product2.png';
-import product3 from '../images/product3.png';
-import product4 from '../images/product4.png';
-import product5 from '../images/product5.png';
-import product6 from '../images/product6.png';
-import product7 from '../images/product7.png';
-import product8 from '../images/product8.png';
-import product9 from '../images/product9.png';
-import product10 from '../images/mostPopularA.png';
 import cartSymbol from '../images/cartSymbol.png';
 import wishList from '../images/wishList.png';
 import compare from '../images/compare.png';
@@ -183,106 +174,6 @@ const Compare = styled.button`
 
   &:hover {text-decoration: underline}
 `;
-
-
-
-
-
-const products: IProduct[] = [
-  {
-    id: 1,
-    image: product1,
-    name: 'product 1',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-    triangle: 'new',
-  },
-  {
-    id: 2,
-    image: product2,
-    name: 'product 2',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-    triangle: 'sale',
-  },
-  {
-    id: 3,
-    image: product3,
-    name: 'product 3',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 4,
-    image: product4,
-    name: 'product 4',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 5,
-    image: product5,
-    name: 'product 5',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 6,
-    image: product6,
-    name: 'product 6',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-    triangle: 'sale',
-  },
-  {
-    id: 7,
-    image: product7,
-    name: 'product 7',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 8,
-    image: product8,
-    name: 'product 8',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 9,
-    image: product9,
-    name: 'product 9',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-  {
-    id: 10,
-    image: product10,
-    name: 'product 10',
-    price: 1875,
-    color: 'pink',
-    size: 's',
-    quantity: 1,
-  },
-];
 
 
 
