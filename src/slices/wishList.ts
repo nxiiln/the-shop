10b16762cb8c2892f53f8eaf97c65df6f5e9c95d
nxiiln <barbashin.nv@gmail.com>
2022-07-17 06:38:12 +0300

    Created: wishListProductSize, wishListProductColor

--- src/slices/wishList.ts
+++ src/slices/wishList.ts
@@ -9,17 +9,33 @@ export const wishListSlice = createSlice({
   initialState,
   reducers: {
     wishListAdd: (state, action: PayloadAction<IProduct>): void => {
-      if (state.findIndex(product => product.id === action.payload.id) === -1) {
+      if (!state.some(product => product.id === action.payload.id)) {
         state.push(action.payload);
       }
     },
 
     wishListRemove: (state, action: PayloadAction<IProduct>) => {
       return state.filter(product => product.id !== action.payload.id);
+    },
+
+    wishListProductSize: (state, action: PayloadAction<IProduct>): void => {
+      const index = state.findIndex(product => product.id === action.payload.id);
+      state[index].size = action.payload.size;
+    },
+
+    wishListProductColor: (state, action: PayloadAction<IProduct>): void => {
+      const index = state.findIndex(product => product.id === action.payload.id);
+      state[index].color = action.payload.color;
     }
   }
 });
 
 
-export const {wishListAdd, wishListRemove} = wishListSlice.actions;
+export const {
+  wishListAdd,
+  wishListRemove,
+  wishListProductSize,
+  wishListProductColor
+} = wishListSlice.actions;
+
 export default wishListSlice.reducer;
