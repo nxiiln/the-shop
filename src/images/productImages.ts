529aa0e6ee8fbd0f1b53840434793ebe1c35ec4d
nxiiln <barbashin.nv@gmail.com>
2022-07-30 06:32:07 +0300

    productImages.ts created

---
+++ src/images/productImages.ts
@@ -0,0 +1,24 @@
+import product1 from './product1.png';
+import product2 from './product2.png';
+import product3 from './product3.png';
+import product4 from './product4.png';
+import product5 from './product5.png';
+import product6 from './product6.png';
+import product7 from './product7.png';
+import product8 from './product8.png';
+import product9 from './product9.png';
+import product10 from './product10.png';
+
+
+export const productImages: {[key: string]: string} = {
+  product1: product1,
+  product2: product2,
+  product3: product3,
+  product4: product4,
+  product5: product5,
+  product6: product6,
+  product7: product7,
+  product8: product8,
+  product9: product9,
+  product10: product10
+};
