ad2c17f87adf0959cced3619fc80a7bcf78c9218
nxiiln <barbashin.nv@gmail.com>
2022-07-31 00:06:36 +0300

    <BlogPost> modified to get information from data.json and blogPostImages.ts

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,11 +1,14 @@
 import {useState} from 'react';
 import styled from 'styled-components/macro';
 import {mediumScreen, smallScreen, useMediaQuery} from '../mediaQueries';
-import {Link} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import BreadCrumbs from './BreadCrumbs';
-import blogPost2 from '../images/blogPost2.png';
 import BlogCategories from './BlogCategories';
 import BlogLabels from './BlogLabels';
+import PageNotFound from './404';
+import {IBlogPostReview} from '../types/IBlogPostReview';
+import data from '../data.json';
+import {blogPostImages} from '../images/blogPostImages';
 
 
 
@@ -139,19 +142,6 @@ const ReviewDate = styled.span`
   color: var(--color-text-regular);
 `;
 
-const ReviewReplay = styled.button`
-  display: block;
-  margin-left: auto;
-  font-family: var(--font-regular);
-  font-size: 11px;
-  line-height: 1.2;
-  font-weight: 400;
-  color: var(--color-text-main);
-  background: transparent;
-  border: none;
-  cursor: pointer;
-`;
-
 
 // WriteReview
 const WriteReview = styled.form`
@@ -210,163 +200,115 @@ const SubmitReview = styled.button`
 
 
 
-interface Review {
-  id: number;
-  name: string;
-  text: string;
-  date: string;
-}
-
-const reviewsList: Review[] = [
-  {
-    id: 1,
-    name: 'Ami Legge',
-    text: 'Curabitur justo elit, accumsan non interdum a, facilisis vel odio. Pellentesque commodo vulputate nisi id suscipit. Proin dapibus turpis vel rhoncus cursus. Aliquam sit amet gravida sem.',
-    date: '25 Marth 2022'
-  },
-  {
-    id: 2,
-    name: 'Lisa Beck',
-    text: 'Mauris sollicitudin vestibulum nisi, at dignissim quam volutpat in. Nulla id quam velit.',
-    date: '2 April 2022'
-  },
-  {
-    id: 3,
-    name: 'Stefanie Broadhurst',
-    text: 'Vivamus tristique tellus id sapien egestas, id pellentesque felis volutpat. Sed et convallis leo. Donec vitae eros rhoncus, blandit odio ut, bibendum metus.',
-    date: '3 April 2022'
-  }
-];
-
-
-
-
 const BlogPost = (): JSX.Element => {
-  const [reviews, setReviews] = useState<Review []>(reviewsList);
+  const findBlogPostId = (id: string | undefined): number => {
+    if (typeof id === 'string') {
+      return +id.replace(/(post)(\d+$)/, '$2');
+    }
+    return -1;
+  }
+  
+  const blogPostId: number = findBlogPostId(useParams().id);
+  const post = data.blogPosts[blogPostId - 1];
+
+  const [reviews, setReviews] = useState<IBlogPostReview[]>(post.reviews);
   const [name, setName] = useState<string>('');
   const [text, setText] = useState<string>('');
   const screen = useMediaQuery();
 
 
+
   return(
-    <WrapperOuter>
-      <WrapperInner>
-        <BreadCrumbs
-          link={
-            <>
-              <Link to='/'>Home</Link>
-              <span>/</span>
-              <Link to='/blog'>Blog</Link>
-              <span>/</span>
-              <span>Do you realy love a trench coat?</span>
-            </>
-          }
-          gridArea={`1 / 1 / 2 / ${screen.big ? '3' : '1'}`}
-        />
+    <>
+      {blogPostId >= 1 && blogPostId <= 3 ?
+        <WrapperOuter>
+          <WrapperInner>
+            <BreadCrumbs
+              link={
+                <>
+                  <Link to='/'>Home</Link>
+                  <span>/</span>
+                  <Link to='/blog'>Blog</Link>
+                  <span>/</span>
+                  <span>
+                    {post.header[0].toUpperCase()}
+                    {post.header.substring(1)}
+                  </span>
+                </>
+              }
+              gridArea={`1 / 1 / 2 / ${screen.big ? '3' : '1'}`}
+            />
 
-        <PostWrapper>
-          <PostHeader>do you realy love a trench coat?</PostHeader>
-          <PostDate>16 Marth 2022</PostDate>
-          <img src={blogPost2} alt={blogPost2} />
-          <PostDescription>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Aenean pretium accumsan metus vestibulum dignissim. 
-              Aenean et lorem in risus mollis finibus eget nec ipsum. 
-              Curabitur commodo gravida lacus, eget tempus lorem sodales a. 
-              Cras tincidunt blandit pulvinar. Phasellus ut cursus diam. 
-              Aenean ac fringilla magna, et vulputate lectus. 
-              Vestibulum pretium ut massa vitae egestas.
-            </p>
-            <p>
-              Mauris tempus luctus gravida. 
-              Vivamus hendrerit purus commodo diam aliquet, mollis porta dui vestibulum. 
-              Curabitur ultrices facilisis libero id tristique. 
-              Maecenas sagittis elit in ex varius consequat. 
-              Cras condimentum, enim sed volutpat pellentesque, 
-              mauris elit hendrerit neque, vitae volutpat lorem massa a ante. 
-              Quisque id nisi congue, semper ante ultricies, dapibus orci. 
-              Morbi lacinia bibendum massa ac ornare. 
-              Cras pharetra et elit ac efficitur. 
-              Sed vitae ipsum ut odio rutrum facilisis id a ligula. 
-              Interdum et malesuada fames ac ante ipsum primis in faucibus. 
-              Ut sit amet fringilla tortor. 
-              Integer semper dignissim lorem, sit amet molestie lacus mollis et. 
-              Sed a risus ut orci viverra sodales ut in dolor. 
-              Nunc condimentum, ipsum nec eleifend volutpat, 
-              lorem neque pretium massa, quis ultricies odio ligula nec ante.
-            </p>
-            <p>
-              Donec turpis massa, malesuada vel risus at, ullamcorper volutpat purus. 
-              Proin nunc eros, vehicula euismod nisl sit amet, malesuada aliquet felis. 
-              Morbi in congue ligula. Sed elementum, elit id fermentum blandit, 
-              turpis felis pharetra tortor, vel posuere est nisl quis nisi. 
-              Donec dictum eu est et efficitur. Praesent convallis tortor eget nibh pulvinar dapibus. 
-              Phasellus congue nulla efficitur faucibus posuere. 
-              Integer augue ante, efficitur ac mollis mollis, lobortis a velit. 
-              Phasellus ac blandit purus. In in urna eget orci sagittis commodo viverra in felis. 
-              Nullam at ex arcu. Nullam mollis dapibus felis quis pulvinar. 
-              Cras vel lectus nibh. Curabitur sodales, felis consequat scelerisque faucibus, 
-              ex purus commodo urna, eu molestie est nisl nec tortor. 
-              Etiam non semper est, vitae blandit nisl.
-            </p>
-          </PostDescription>
-        </PostWrapper>
+            <PostWrapper>
+              <PostHeader>{post.header}</PostHeader>
+              <PostDate>{post.date}</PostDate>
 
-        <ReviewsWrapper>
-          <ReviewsNumber>
-            3 REVIEWS
-          </ReviewsNumber>
-
-          {reviews.map((review: Review) =>
-            <Review key={review.id}>
-              <Reviewname>{review.name}</Reviewname>
-              <ReviewText>{review.text}</ReviewText>
-              <ReviewDate>{review.date}</ReviewDate>
-              <ReviewReplay type='button'>Replay</ReviewReplay>
-            </Review>
-          )}
-
-          <WriteReview onSubmit={(e: React.FormEvent<HTMLFormElement>): void => {
-            e.preventDefault();
-            const newReviews: Review[] = [...reviews];
-            const date: Date = new Date();
-            const newReview: Review = {
-              id: reviews[reviews.length-1].id + 1,
-              name: name,
-              text: text,
-              date: `${date.getDate()} ${date.getMonth()} ${date.getFullYear()}`
-            };
-            newReviews.push(newReview);
-            setReviews(newReviews);
-          }}>
-            <WriteReviewHeader>WRITE REVIEW</WriteReviewHeader>
-
-            <Label>
-              NAME
-              <InputText
-                type='text'
-                value={name}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setName(e.target.value)}
+              <img
+                src={blogPostImages[`blogPost${post.id}`]}
+                alt={'blog post'}
               />
-            </Label>
 
-            <Label>
-              REVIEW TEXT
-              <InputTextArea
-                value={text}
-                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>): void => setText(e.target.value)}
-              />
-            </Label>
-            
-            <SubmitReview type='submit'>SUBMIT REVIEW</SubmitReview>
-          </WriteReview>
-        </ReviewsWrapper>
+              <PostDescription>
+                {post.text.map((p: string): JSX.Element => <p key={p.substring(0, 16)}>{p}</p>)}
+              </PostDescription>
+            </PostWrapper>
 
-        <BlogCategories gridArea={screen.big ? '2 / 2 / 3 / 3' : '2 / 1 / 3 / 2'} />
-        {screen.big && <BlogLabels gridArea='3 / 2 / 4 / 3' />}
-      </WrapperInner>
-    </WrapperOuter>
+
+            <ReviewsWrapper>
+              <ReviewsNumber>{reviews.length} REVIEWS</ReviewsNumber>
+
+              {reviews.map((review: IBlogPostReview) =>
+                <Review key={review.id}>
+                  <Reviewname>{review.name}</Reviewname>
+                  <ReviewText>{review.text}</ReviewText>
+                  <ReviewDate>{review.date}</ReviewDate>
+                </Review>
+              )}
+
+              <WriteReview onSubmit={(e: React.FormEvent<HTMLFormElement>): void => {
+                e.preventDefault();
+                const newReviews: IBlogPostReview[] = JSON.parse(JSON.stringify(reviews));
+                const date: Date = new Date();
+                const newReview: IBlogPostReview = {
+                  id: reviews[reviews.length-1].id + 1,
+                  name: name,
+                  text: text,
+                  date: `${date.getDate()} ${date.getMonth()} ${date.getFullYear()}`
+                };
+                newReviews.push(newReview);
+                setReviews(newReviews);
+              }}>
+                <WriteReviewHeader>WRITE REVIEW</WriteReviewHeader>
+
+                <Label>
+                  NAME
+                  <InputText
+                    type='text'
+                    value={name}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void => setName(e.target.value)}
+                  />
+                </Label>
+
+                <Label>
+                  REVIEW TEXT
+                  <InputTextArea
+                    value={text}
+                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>): void => setText(e.target.value)}
+                  />
+                </Label>
+                
+                <SubmitReview type='submit'>SUBMIT REVIEW</SubmitReview>
+              </WriteReview>
+            </ReviewsWrapper>
+
+            <BlogCategories gridArea={screen.big ? '2 / 2 / 3 / 3' : '2 / 1 / 3 / 2'} />
+            {screen.big && <BlogLabels gridArea='3 / 2 / 4 / 3' />}
+          </WrapperInner>
+        </WrapperOuter>
+        :
+        <PageNotFound />
+      }
+    </>
   );
 }
 
