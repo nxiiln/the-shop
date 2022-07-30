d40680f315c5869fb01b7b3f1cd6973abdfd710b
nxiiln <barbashin.nv@gmail.com>
2022-07-31 00:01:43 +0300

    blogPostImages.ts

---
+++ src/images/blogPostImages.ts
@@ -0,0 +1,9 @@
+import blogPost1 from './blogPost1.png';
+import blogPost2 from './blogPost2.png';
+import blogPost3 from './blogPost3.png';
+
+export const blogPostImages: {[key: string]: string} = {
+  blogPost1: blogPost1,
+  blogPost2: blogPost2,
+  blogPost3: blogPost3
+};
