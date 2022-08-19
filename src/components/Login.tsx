3af5aa992cac8084ae0f4cbc22ad173668b8bb3e
nxiiln <barbashin.nv@gmail.com>
2022-08-19 22:55:09 +0300

    <Login> checks email and adds email to store.account

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,9 @@
+import {useState} from 'react';
 import styled from 'styled-components/macro';
 import {smallScreen} from '../mediaQueries';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../redux-hooks';
+import {accountChange} from '../slices/account';
 import BreadCrumbs from './BreadCrumbs';
 
 
@@ -103,7 +106,8 @@ const TextBold = styled.span`
   color: var(--color-text-main);
 `;
 
-const Label = styled.label`
+const Label = styled.label<{error?: boolean}>`
+  position: relative;
   height: 45px;
   margin-bottom: 15px;
   display: grid;
@@ -117,9 +121,15 @@ const Label = styled.label`
   > input {
     width: 254px;
     height: 30px;
-    border: 1px solid var(--color-border);
+    border: 1px solid ${props => !props.error ?
+      'var(--color-border)' : 'var(--color-input-error)'
+    };
 
-    &:focus {outline: 1px solid #000}
+    &:focus {
+      outline: 1px solid ${props => !props.error ?
+        'var(--color-input-outline)' : 'var(--color-input-error)'
+      };
+    }
   }
 `;
 
@@ -167,82 +177,128 @@ const NewCustomers = styled.div`
   @media ${smallScreen} {margin: 0}
 `;
 
-const ButtonCreateAccount = styled(Link)`
+const ButtonCreateAccount = styled.button`
   ${ButtonPreset}
   width: 254px;
   display: inline-flex;
   justify-content: center;
   align-items: center;
   text-decoration: none;
+  cursor: pointer;
 
   &:hover {background: var(--color-button-solid-hover)}
+`;
+
+const Error = styled.span`
+  position: absolute;
+  top: 47px;
+  left: 0;
+  font-family: var(--font-regular);
+  font-size: 11px;
+  color: #f00;
 `;
 
 
 
 
-const Login = (): JSX.Element => (
-  <WrapperOuter>
-    <WrapperInner>
-      <BreadCrumbs
-        link={
-          <>
-            <Link to='/'>Home</Link>
-            <span>/</span>
-            <span>Login</span>
-          </>
-        }
-        marginBottom='20px'
-      />
+const Login = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [emailNew, setEmailNew] = useState<string>('');
+  const [emailNewError, setEmailNewError] = useState<boolean>(false);
 
-      <UserLogin>
-        <TitleWrapper>
-          <Title>USER LOGIN</Title>
-        </TitleWrapper>
-        
-        <Groups>
-          <RegisteredCustomers>
-            <TextUp>REGISTERED CUSTOMERS</TextUp>
-            <Text>
-              <TextBold>Already registered? </TextBold>
-              Please log in below:
-            </Text>
+  return(
+    <WrapperOuter>
+      <WrapperInner>
+        <BreadCrumbs
+          link={
+            <>
+              <Link to='/'>Home</Link>
+              <span>/</span>
+              <span>Login</span>
+            </>
+          }
+          marginBottom='20px'
+        />
 
-            <form onSubmit={(e: React.FormEvent<HTMLFormElement>): void => e.preventDefault()}>
-              <Label>
-                E-MAIL*
-                <input type='email' required />
-              </Label>
+        <UserLogin>
+          <TitleWrapper>
+            <Title>USER LOGIN</Title>
+          </TitleWrapper>
+          
+          <Groups>
+            <RegisteredCustomers>
+              <TextUp>REGISTERED CUSTOMERS</TextUp>
+              <Text>
+                <TextBold>Already registered? </TextBold>
+                Please log in below:
+              </Text>
 
-              <Label>
-                PASSWORD*
-                <input type='password' required />
-              </Label>
+              <form onSubmit={(e: React.FormEvent<HTMLFormElement>): void => e.preventDefault()}>
+                <Label>
+                  E-MAIL*
+                  <input type='email' required />
+                </Label>
 
-              <ButtonLogin>LOGIN</ButtonLogin>
-              <ButtonUnderline type='button'>Forgot your password?</ButtonUnderline>
-            </form>
-          </RegisteredCustomers>
+                <Label>
+                  PASSWORD*
+                  <input type='password' required />
+                </Label>
 
-          <NewCustomers>
-            <TextUp>NEW CUSTOMERS</TextUp>
-            <Text>
-              <TextBold>Enter your email address to create an account:</TextBold>
-            </Text>
+                <ButtonLogin>LOGIN</ButtonLogin>
+                <ButtonUnderline type='button'>Forgot your password?</ButtonUnderline>
+              </form>
+            </RegisteredCustomers>
 
-            <form onSubmit={(e: React.FormEvent<HTMLFormElement>): void => e.preventDefault()}>
-              <Label>
-                E-MAIL*
-                <input type='email' required />
-              </Label>
-              <ButtonCreateAccount to='/create-account'>CREATE AN ACCOUNT</ButtonCreateAccount>
-            </form>
-          </NewCustomers>
-        </Groups>
-      </UserLogin>
-    </WrapperInner>
-  </WrapperOuter>
-);
+
+            <NewCustomers>
+              <TextUp>NEW CUSTOMERS</TextUp>
+              <Text>
+                <TextBold>Enter your email address to create an account:</TextBold>
+              </Text>
+
+              <form
+                noValidate
+                onSubmit={(e: React.FormEvent<HTMLFormElement>): void => {
+                  e.preventDefault();
+                  if (e.currentTarget.checkValidity()) {
+                    dispatch(accountChange({email: emailNew}));
+                    navigate('/create-account');
+                  }
+                }}
+              >
+                <Label error={emailNewError}>
+                  E-MAIL*
+                  <input
+                    type='email'
+                    pattern='.+@.+\..+'
+                    required
+                    placeholder='your@email.com'
+                    value={emailNew}
+
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void => {
+                      setEmailNew(e.target.value);
+                      e.target.validity.valid && setEmailNewError(false);
+                    }}
+                    
+                    onBlur={(e: React.FocusEvent<HTMLInputElement>): void => {
+                      if (emailNew && !e.target.validity.valid) setEmailNewError(true);
+                    }}
+
+                    onInvalid={(): void => setEmailNewError(true)}
+                  />
+                  <Error>{emailNewError && 'Please enter a valid email address'}</Error>
+                </Label>
+
+                <ButtonCreateAccount type='submit'>CREATE AN ACCOUNT</ButtonCreateAccount>
+              </form>
+            </NewCustomers>
+          </Groups>
+        </UserLogin>
+      </WrapperInner>
+    </WrapperOuter>
+  );
+}
 
 
 
