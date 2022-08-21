9e11375731f1b7b8bc4986ab937685ae550090fa
nxiiln <barbashin.nv@gmail.com>
2022-08-21 22:24:21 +0300

    action added: accountChangeMyPersonalInfo

--- src/slices/account.ts
+++ src/slices/account.ts
@@ -1,6 +1,14 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {IAccount} from '../types/IAccount';
 
+interface IMyPersonalInfo {
+  isActive?: boolean;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  newsletterSubscription?: boolean;
+}
 
 const initialState: IAccount[] = [];
 
@@ -8,12 +16,24 @@ export const accountSlice = createSlice({
   name: 'account',
   initialState,
   reducers: {
-    accountChange: (state, action: PayloadAction<IAccount>): void => {
+    accountCreate: (state, action: PayloadAction<IAccount>): void => {
       state.push(action.payload);
+    },
+
+    accountChangeMyPersonalInfo: (state, action: PayloadAction<IMyPersonalInfo>): void => {
+      const account = state[state.findIndex(
+        (account: IAccount): boolean | undefined => account?.isActive
+      )];
+      
+      account.firstName = action.payload.firstName;
+      account.lastName = action.payload.lastName;
+      account.email = action.payload.email;
+      account.password = action.payload.password;
+      account.newsletterSubscription = action.payload.newsletterSubscription;
     }
   }
 });
 
 
-export const {accountChange} = accountSlice.actions;
+export const {accountCreate, accountChangeMyPersonalInfo} = accountSlice.actions;
 export default accountSlice.reducer;
