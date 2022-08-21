d298e19c4adc0dfd84f0e7d5a61de727a3f2d0a2
nxiiln <barbashin.nv@gmail.com>
2022-08-21 23:13:50 +0300

    accountSlice adapted for IAccounts

--- src/slices/account.ts
+++ src/slices/account.ts
@@ -1,28 +1,32 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {IAccount} from '../types/IAccount';
+import {IAccounts} from '../types/IAccounts';
 
 interface IMyPersonalInfo {
-  isActive?: boolean;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  password?: string;
-  newsletterSubscription?: boolean;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  newsletterSubscription: boolean;
 }
 
-const initialState: IAccount[] = [];
+const initialState: IAccounts = {newEmail: '', accounts: []};
 
 export const accountSlice = createSlice({
   name: 'account',
   initialState,
   reducers: {
+    accountSetNewEmail: (state, action: PayloadAction<string>): void => {
+      state.newEmail = action.payload;
+    },
+
     accountCreate: (state, action: PayloadAction<IAccount>): void => {
-      state.push(action.payload);
+      state.accounts.push(action.payload);
     },
 
     accountChangeMyPersonalInfo: (state, action: PayloadAction<IMyPersonalInfo>): void => {
-      const account = state[state.findIndex(
-        (account: IAccount): boolean | undefined => account?.isActive
+      const account = state.accounts[state.accounts.findIndex(
+        (account: IAccount): boolean => account.isActive
       )];
       
       account.firstName = action.payload.firstName;
@@ -35,5 +39,5 @@ export const accountSlice = createSlice({
 });
 
 
-export const {accountCreate, accountChangeMyPersonalInfo} = accountSlice.actions;
+export const {accountSetNewEmail, accountCreate, accountChangeMyPersonalInfo} = accountSlice.actions;
 export default accountSlice.reducer;
