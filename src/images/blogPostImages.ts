4812a0a60a0686af63be85300b33dad15b89460b
nxiiln <barbashin.nv@gmail.com>
2022-09-02 21:18:24 +0300

    Images replaced

--- src/images/blogPostImages.ts
+++ src/images/blogPostImages.ts
@@ -1,6 +1,6 @@
-import blogPost1 from './blogPost1.png';
-import blogPost2 from './blogPost2.png';
-import blogPost3 from './blogPost3.png';
+import blogPost1 from './blogPost1.jpg';
+import blogPost2 from './blogPost2.jpg';
+import blogPost3 from './blogPost3.jpg';
 
 export const blogPostImages: {[key: string]: string} = {
   blogPost1: blogPost1,
