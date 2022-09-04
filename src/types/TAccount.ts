9f984d70e520633edcdd72d8788b5fff6a209ede
nxiiln <barbashin.nv@gmail.com>
2022-09-05 00:08:12 +0300

    Added orders

--- src/types/TAccount.ts
+++ src/types/TAccount.ts
@@ -1,4 +1,8 @@
 import {IAddressInfo} from "./IAddressInfo";
 import {IPersonalInfo} from "./IPersonalInfo";
+import {IOrder} from "./IOrder";
 
-export type TAccount = IPersonalInfo & IAddressInfo & {isActive: boolean};
+export type TAccount = IPersonalInfo & IAddressInfo & {
+  isActive: boolean;
+  orders?: IOrder[];
+};
