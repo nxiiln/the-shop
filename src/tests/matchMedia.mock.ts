8dc12995dfbe7647dce6db24c2dbcb4d0eeba678
nxiiln <barbashin.nv@gmail.com>
2022-11-13 18:51:40 +0300

    Created matchMediaMock

---
+++ src/tests/matchMedia.mock.ts
@@ -0,0 +1,10 @@
+export const matchMediaMock = window.matchMedia = query => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
